5fdab9c9d9f5c9de533feec7113b4b4095882518
hishmaiyoob <hishmahish13@gmail.com>
2026-08-03 10:16:50 +0530

    Added features to order online page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
-import OrderOnlineEmpty from "./pages/OrderOnlineEmpty";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import OrderOnline from "./pages/OrderOnline";
 
 function App() {
-  return <OrderOnlineEmpty />;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <OrderOnline />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
