20c38248ecaf79f6585cb465e20096ce0647d55a
hishmaiyoob <hishmahish13@gmail.com>
2026-08-10 20:57:19 +0530

    made few changes

--- src/data/QueueStats.js
+++ src/data/QueueStats.js
@@ -24,8 +24,3 @@ export const QueueStats = [
     valueColor: "text-[#27ae60]",
   },
 ];
-
-/**
- * 
- * please ive correct fix for below problem...     there is a dashboard.. inthen left side has sidebar and rest of the screen has dashboard contents...  i have in 100% screen size... sidebar is scrollable.. bcz sidebar is longer... and dashboard contents are longer that sidebar... so if i scroll the screen 2 sections were scrilling and bottom of the sidebar there is a white space... what i want is if i scroll the screen both contents should scroll.. but if the sidebar is reaches the height is should not scroll... the dashboard contents should scroll.. i think you got me.. give better solution for this...    
- */
