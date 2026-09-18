2ca832c4df4d7d666d0c81a5c877d60d59338a29
Mahdhe <mmmahdhe@gmail.com>
2026-09-18 14:44:23 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
@@ -7,8 +7,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Home />
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </>
   );
