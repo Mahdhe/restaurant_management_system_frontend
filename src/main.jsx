3dbb2053210dfea0f31ce779431e63d9b59620f4
Nithes Kunasekaram <nitheskunasekaram@gmail.com>
2026-09-22 10:03:09 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
